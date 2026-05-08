69b139810bf7fa0b44d1e3bce5bfeb3e27a83fdc
Cristina Pérez Grau <cperezgr@uoc.edu>
2026-05-08 20:40:51 +0200

    cambios formatos

--- fira-totssants/src/assets/scripts/main.js
+++ fira-totssants/src/assets/scripts/main.js
@@ -11,14 +11,13 @@
  */
 
 import "@fortawesome/fontawesome-free/css/all.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "bootstrap";
+
+AOS.init();
 
 +(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 })();
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
